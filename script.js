d77028a5ad5f85f322772063d44e46e0fdf22b76
Tanaphat Saeung <tanaphat.sa2@gmail.com>
2025-06-08 16:51:42 +0700

    Finish the projectg converting currency

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const amount_one = document.getElementById('amount-one')
 const amount_two = document.getElementById('amount-two')
 
 const rateText = document.getElementById('rate')
-const btn = document.getElementById('btn')
+const swap = document.getElementById('btn')
 
 const calculateMoney = () =>{
     const one = currency_one.value
@@ -16,9 +16,18 @@ const calculateMoney = () =>{
     then(data=>{
         const rate = data['conversion_rates'][two]
         rateText.innerText = ` 1 ${one} = ${rate} ${two}`
+        amount_two.value = (amount_one.value*rate).toFixed(2)
     })
 }
 
 currency_one.addEventListener('change',calculateMoney)
 currency_two.addEventListener('change',calculateMoney)
+amount_one.addEventListener('input',calculateMoney)
+amount_two.addEventListener('input',calculateMoney)
+swap.addEventListener('click',()=>{
+    const tempCurrency = currency_one.value
+    currency_one.value=currency_two.value
+    currency_two.value = tempCurrency
+    calculateMoney()
+})
 calculateMoney()
