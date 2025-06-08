992df360acf9617819b79169151091e952d6b168
Tanaphat Saeung <tanaphat.sa2@gmail.com>
2025-06-08 16:50:59 +0700

    finish

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ const amount_two = document.getElementById('amount-two')
 
 const rateText = document.getElementById('rate')
 const btn = document.getElementById('btn')
+
+const calculateMoney = () =>{
+    const one = currency_one.value
+    const two = currency_two.value
+    let url = `https://v6.exchangerate-api.com/v6/6ee25e7acc71b1f9761688e5/latest/${one}`
+    fetch(url).
+    then(res=>res.json()).
+    then(data=>{
+        const rate = data['conversion_rates'][two]
+        rateText.innerText = ` 1 ${one} = ${rate} ${two}`
+    })
+}
+
+currency_one.addEventListener('change',calculateMoney)
+currency_two.addEventListener('change',calculateMoney)
+calculateMoney()
