ba2164d47e9a76287646356afa1bc5b633bd61b7
Tanaphat Saeung <tanaphat.sa2@gmail.com>
2025-06-08 16:27:13 +0700

    Finished the  connection to API's converting currency

---
+++ script.js
@@ -0,0 +1,8 @@
+const currency_one =document.getElementById('currency-one')
+const currency_two =document.getElementById('currency-two')
+
+const amount_one = document.getElementById('amount-one')
+const amount_two = document.getElementById('amount-two')
+
+const rateText = document.getElementById('rate')
+const btn = document.getElementById('btn')
